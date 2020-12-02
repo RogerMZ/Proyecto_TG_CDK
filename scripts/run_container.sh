28d1047318ca8078cc0c0fc8f6c1e27df4a3c9f1
RogerMZ <rogermz.projects@gmail.com>
2020-12-01 19:52:53 -0600

    Agregando scripts de ejecución y test

---
+++ scripts/run_container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name proyapi -itd --rm -p 5000:5000 rogermz/proyectoapi:1.1
+
---
+++ scripts/test_cont.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl localhost:5000/
+
+# Test: funcion de Susana
+curl http://138.68.2.24/saludo/jenkis
+
+# Test: funcion de Jose
+curl http://138.68.2.24/cuadrado/2.7 
+#bash script_name_to_test.sh
+
+
