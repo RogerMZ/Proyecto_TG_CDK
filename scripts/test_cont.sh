838b193c47928302475f64769ee0082496316210
RogerMZ <rogermz.projects@gmail.com>
2020-12-01 20:10:10 -0600

    ajustando el puerto en el test

--- scripts/test_cont.sh
+++ scripts/test_cont.sh
@@ -3,10 +3,10 @@
 curl localhost:5000/
 
 # Test: funcion de Susana
-curl http://138.68.2.24/saludo/jenkis
+curl http://138.68.2.24:5000/saludo/jenkis
 
 # Test: funcion de Jose
-curl http://138.68.2.24/cuadrado/2.7 
+curl http://138.68.2.24:5000/cuadrado/2.7 
 #bash script_name_to_test.sh
 
 
